321c1ccf9de7964d5b28648d9e8c1b6d2e9800b6
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-09-11 19:31:11 +0200

    metodos catalogar

--- main.py
+++ main.py
@@ -31,15 +31,23 @@ class Motocicleta(Bicicleta):
 
 def catalogar(lista):
     for vehiculo in lista:
-        print(type(vehiculo), vehiculo.__dict__)
+        print(type(vehiculo).__name__, vehiculo.__dict__)
 
-if __name__=="__main__":
+def catalogar1(lista, n_ruedas = None):
+    contador = 0
+    for vehiculo in lista:
+        if n_ruedas == vehiculo.ruedas:
+            print(vehiculo)
+            contador += 1
+    if n_ruedas:
+        print(f"Se han encontrado {contador} vehículos con {n_ruedas} ruedas:")
 
-    a = Camioneta("marron", 8, 120, 400, 500)
-    b = Coche("verde", 4, 120, 800)
-    c = Bicicleta("azul", 2, "triciclo")
-    d = Coche("rosado", 4, 150, 800)
-    e = Motocicleta("azul", 2, "triciclo", 150, 200)
+a = Camioneta("marron", 8, 120, 400, 500)
+b = Coche("verde", 4, 120, 800)
+c = Bicicleta("azul", 2, "triciclo")
+d = Coche("rosado", 4, 150, 800)
+e = Motocicleta("azul", 2, "triciclo", 150, 200)
 
-    lista_vehiculos = [a, b, c, d, e]
-    catalogar(lista_vehiculos)
+lista_vehiculos = [a, b, c, d, e]
+catalogar(lista_vehiculos)
+catalogar1(lista_vehiculos)
